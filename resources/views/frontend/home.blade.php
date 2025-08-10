301828b0b0f8b2c64cb6df8482fa950b394861df
firozkabir25 <firozkabirprodhan5@gmail.com>
2025-08-11 00:04:39 +0600

    Initial commit with templeting on larvel

---
+++ resources/views/frontend/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.app')
+@section('content')
+    @include('frontend.partials.hero_section')
+    @include('frontend.partials.featured')
+    <!-- Main Content -->
+    @include('frontend.partials.recent')
+    
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AuthController;
+
+// Route::get('/', function () {
+//     return view('frontend.home');
+// });
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('single-blog/{id}', [HomeController::class, 'show'])->name('single-blog');
+Route::get('category/web-development', [HomeController::class, 'categories'])->name('category.web-development');
+Route::get('login', [AuthController::class, 'index'])->name('login');
+Route::get('registration', [AuthController::class, 'create'])->name('registration');
